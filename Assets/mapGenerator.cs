3c7ba2a6dbf0457c4d955c0a5dc8e5512e09653a
nem-e5i5 <alexandre.thouvenin@outlook.com>
2016-05-11 15:59:24 +0200

    Ancien repository

---
+++ Assets/mapGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var go= GameObject.Find("00");
+        var component = this.GetComponent<GridMap>();
+        for (int i =0; i < 100; ++i)
+        {
+            component.tiles[i] = go.GetComponent<Tile>();
+            System.Console.WriteLine(component.tiles[i].ToString());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
