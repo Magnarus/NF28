3004035fd4d526d7e2947ce8f2691571a74642b8
nem-e5i5 <alexandre.thouvenin@outlook.com>
2016-06-01 11:58:12 +0200

    Test de correction sur les Equipements: maintenant un enchantement peut agir sur la base (épée rare préenchantée)ou bien sur l'instance (dague empoisonnée par le joueur)

--- Assets/EquipmentInstance.cs
+++ Assets/EquipmentInstance.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using System.Reflection;
 
 public class EquipmentInstance
 {
@@ -32,3 +33,81 @@ public class EquipmentInstance
     }
 }
 
+/*
+ * Equipment _modifieditem = new Equipment();
+    public Equipment modifiedItem
+    {
+        get
+        {
+            foreach (var prop in properties)
+            {
+                var currentValue = parseProperty(baseItem, prop.Key);
+                //TODO: null is bad. find another special flag
+                if (currentValue != null)
+                    parseProperty(_modifieditem, prop.Key, prop.Value);
+            }
+
+            return _modifieditem;
+        }
+    }
+
+    object parseProperty(object source, string key, object value = null)
+    {
+        if (key.Contains("."))
+        {
+            var split = key.Split('.');
+            var first = split[0];
+            var next = string.Join(".", split.Skip(1).ToArray());
+            var currentobj = parseProperty(source, first);
+            if (currentobj == null)
+                return currentobj;
+            else
+                return parseProperty(currentobj, next, value);
+        }
+        else
+        {
+            var myType = source.GetType();
+            if (key.StartsWith("`"))
+            {
+                key = key.Substring(1);
+
+                //TODO omygad that's slow
+                var type = AppDomain.CurrentDomain.GetAssemblies()
+                                   .SelectMany(x => x.GetTypes())
+                                   .FirstOrDefault(x => x.Name == "MyClass");
+
+                return ((MonoBehaviour)source).GetComponent(type);
+            }
+            else
+            {
+                var member = myType.GetMember(key);
+
+                if (member.Length > 1)
+                    System.Console.WriteLine("Warning: membre multiple détecté pour la substitution de " + key);
+
+                if (member.Length <= 0)
+                    return null;
+
+                var myMember = member.First();
+                switch (myMember.MemberType)
+                {
+                    case MemberTypes.Field:
+                        if (value == null)
+                            return ((FieldInfo)myMember).GetValue(source);
+                        else
+                            ((FieldInfo)myMember).SetValue(source, value);
+                        break;
+                    case MemberTypes.Property:
+                        if (value == null)
+                            return ((PropertyInfo)myMember).GetValue(source, new object[] { });
+                        else
+                            ((PropertyInfo)myMember).SetValue(source, value, new object[] { });
+                        break;
+
+                        //TODO: overloading functions
+                }
+                return null;
+            }
+        }
+    }
+*/
