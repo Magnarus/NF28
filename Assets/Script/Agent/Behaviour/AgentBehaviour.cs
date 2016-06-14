30e54e62af6c953598f51fc66586965b08f69769
magnarus <delau.greg@gmail.com>
2016-06-14 22:14:26 +0200

    fix AgentBehaviour class

---
+++ Assets/Script/Agent/Behaviour/AgentBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AgentBehaviour {
+
+	private Agent mParent;
+	public Agent MParent {
+		get { return mParent; }
+		set { mParent = value; }
+	}
+
+	public abstract bool Run();
+
+	public virtual bool finish() {
+		return true;
+	}
+}
