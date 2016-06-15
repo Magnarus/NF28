3facc19d1b8c9a9f832a0c2b7e937d1206be530f
Vincent Keller <vincent-keller@hotmail.fr>
2016-06-15 13:12:38 +0200

    ArcherAttackBehaviour done & fix

--- Assets/Script/Agent/Behaviour/AttackBehaviour.cs
+++ Assets/Script/Agent/Behaviour/AttackBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Descriptors;
 using System.Collections.Generic;
 
 public class AttackBehaviour : AgentBehaviour {
@@ -17,7 +18,45 @@ public class AttackBehaviour : AgentBehaviour {
 
 
 	public override CreatureAction Run() {
-		return null;
+		List<Creature> ennemiesJ1 = Parent.controller.creaturesJ1;
+		List<PhysicTile> tiles = new List<PhysicTile> ();
+		tiles = GetTilesInRange ();
+
+		List<PhysicTile> ennemies = GetEnnemyInRange (tiles);
+		float damages = 0; 
+		float maxDamage = 0;
+		Creature targetDeath = null;
+		Creature targetDamages = null;
+
+		CreatureDescriptor statsCreature = ((AgentCreature)Parent).CurrentCreature.GetComponent<CreatureDescriptor>();
+
+		foreach (PhysicTile e in ennemies) {
+			Creature newC = e.contentTile.GetComponent<Creature> ();
+			if (ennemiesJ1.Contains (newC)) {
+				CreatureDescriptor statsEnnemy = newC.GetComponent<CreatureDescriptor> ();
+				damages += (20 + statsCreature.Strength.value - statsEnnemy.Armor.value);
+
+				if (statsEnnemy.HP.CurrentValue - damages <= 0) {
+					if (targetDeath == null || newC.classCreature == "hero") {
+						targetDeath = newC;
+					}
+				} else if (damages > maxDamage) {
+					maxDamage = damages;
+					targetDamages = newC;
+				}
+			}
+		}
+
+		CreatureAction c;
+		if (targetDeath != null) {
+			c = new CreatureAction (ActionType.ATK, ((AgentCreature)Parent).CurrentCreature, GetDirectionToGo (targetDeath.tile), targetDeath, 500); 
+		} else if (targetDamages != null) {
+			c = new CreatureAction (ActionType.ATK, ((AgentCreature)Parent).CurrentCreature, GetDirectionToGo (targetDamages.tile), targetDamages, maxDamage); 
+		} else {
+			c = new CreatureAction (ActionType.ATK, ((AgentCreature)Parent).CurrentCreature);
+		}
+
+		return c;
 	}
 
 
--- Assets/Script/Agent/Behaviour/WarriorAttackBehaviour.cs
+++ Assets/Script/Agent/Behaviour/WarriorAttackBehaviour.cs
@@ -8,49 +8,6 @@ public class WarriorAttackBehaviour : AttackBehaviour {
 	public WarriorAttackBehaviour(AgentCreature agent) : base(agent) {	}
 
 
-	/** Calcul du meilleur coup **/
-	public override CreatureAction Run() {
-		List<Creature> ennemiesJ1 = Parent.controller.creaturesJ1;
-		List<PhysicTile> tiles = new List<PhysicTile> ();
-		tiles = GetTilesInRange ();
-
-		List<PhysicTile> ennemies = GetEnnemyInRange (tiles);
-		float damages = 0; 
-		float maxDamage = 0;
-		Creature targetDeath = null;
-		Creature targetDamages = null;
-
-		CreatureDescriptor statsCreature = ((AgentCreature)Parent).CurrentCreature.GetComponent<CreatureDescriptor>();
-
-		foreach (PhysicTile e in ennemies) {
-			Creature newC = e.contentTile.GetComponent<Creature> ();
-			if (ennemiesJ1.Contains (newC)) {
-				CreatureDescriptor statsEnnemy = newC.GetComponent<CreatureDescriptor> ();
-				damages += (20 + statsCreature.Strength.value - statsEnnemy.Armor.value);
-
-				if (statsEnnemy.HP.CurrentValue - damages <= 0) {
-					if (targetDeath == null || newC.classCreature == "hero") {
-						targetDeath = newC;
-					}
-				} else if (damages > maxDamage) {
-					maxDamage = damages;
-					targetDamages = newC;
-				}
-			}
-		}
-
-		CreatureAction c;
-		if (targetDeath != null) {
-			c = new CreatureAction (ActionType.ATK, ((AgentCreature)Parent).CurrentCreature, GetDirectionToGo (targetDeath.tile), targetDeath, 500); 
-		} else if (targetDamages != null) {
-			c = new CreatureAction (ActionType.ATK, ((AgentCreature)Parent).CurrentCreature, GetDirectionToGo (targetDamages.tile), targetDamages, maxDamage); 
-		} else {
-			c = new CreatureAction (ActionType.ATK, ((AgentCreature)Parent).CurrentCreature);
-		}
-
-		return c;
-	}
-
 	/** Retourne une des cases à portée d'attaque du warrior pour attaquer sa cible **/
 	public override PhysicTile GetDirectionToGo(PhysicTile t) {
 		PhysicTile destination = null;
